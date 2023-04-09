5fcf77313d508cca0ccffb0d4820591b12ff3b8e
vuong.nvv <vuon.co.g@gmail.com>
2023-04-09 22:19:47 +0700

    [Completed] Calendar Timeline Logic 🗓

--- app/src/main/java/com/example/taskmanagementapp/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagementapp/MainActivity.kt
@@ -1,19 +1,12 @@
 package com.example.taskmanagementapp
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.Network
-import android.net.NetworkInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.taskmanagementapp.ViewModel.LogInViewModel
 import com.example.taskmanagementapp.navigation.RootNavigationGraph
-import com.example.taskmanagementapp.ui.theme.ChangeLightness
-import com.example.taskmanagementapp.ui.theme.Primary4
 import com.example.taskmanagementapp.ui.theme.TaskManagementAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 

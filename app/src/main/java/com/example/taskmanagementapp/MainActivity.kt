224143ae9e8ef8673db503fd51bebfb89e328981
phucminhtran2002@gmail.com <phucminhtran2002@gmail.com>
2023-04-11 16:49:48 +0700

    Google Sign In
    (cherry picked from commit 3843c3f9eb355f5fb257e6abc39a404efcb81ea1)
    
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/example/taskmanagementapp/MainActivity.kt
    #	app/src/main/java/com/example/taskmanagementapp/navigation/BotNavGraph.kt
    #	app/src/main/java/com/example/taskmanagementapp/ui/screens/MainScreen.kt
    #	app/src/main/java/com/example/taskmanagementapp/ui/screens/auth/LoginContent.kt

--- app/src/main/java/com/example/taskmanagementapp/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagementapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.taskmanagementapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.taskmanagementapp.ViewModel.LogInViewModel
@@ -13,14 +16,20 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private lateinit var navController: NavHostController
+    private val logInViewModel = LogInViewModel(this)
+    private val signInLauncher = registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
+        logInViewModel.handleSignInResult(result.data)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             navController = rememberNavController()
             TaskManagementAppTheme {
-                RootNavigationGraph(navController = navController, logInViewModel = LogInViewModel(this))
+                RootNavigationGraph(navController = navController, logInViewModel = logInViewModel)
             }
         }
     }
+
+    fun getSignInLauncher() : ActivityResultLauncher<IntentSenderRequest> = signInLauncher
 }
